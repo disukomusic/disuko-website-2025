eefe6c12736f8f0fcffd9d9840566c8830ed00e4
Disuko <64217407+disukomusic@users.noreply.github.com>
2025-06-24 09:26:12 -0400

    Revert "latest commit componenent"
    This reverts commit 17d841e45b9013aec6ea35cdab3175355a985baf.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,6 @@ import Head from "next/head";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-import { initPlasmicLoader } from '@plasmicapp/loader-react';
-
-export const PLASMIC = initPlasmicLoader({
-    projects: [
-        {
-            id: 'x4VgG6kzZCVuaqknYN7tgc', // get this from Plasmic dashboard
-            token: '', // optional, if you have private projects
-        }
-    ],
-});
-
-
-import { LatestCommit } from '@/components/LatestCommit';
-
-PLASMIC.registerComponent(LatestCommit, {
-    name: 'LatestCommit',
-    props: {}
-});
-
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
